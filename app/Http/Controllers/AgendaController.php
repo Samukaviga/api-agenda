28e4309e32f5626bc1fc854d2ffd3b5a5e837cb3
samuel <samuelgomes101112@gmail.com>
2025-06-30 16:33:53 -0300

    updated method post agendamento

--- app/Http/Controllers/AgendaController.php
+++ app/Http/Controllers/AgendaController.php
@@ -4,41 +4,66 @@ namespace App\Http\Controllers;
 
 use App\Models\Agenda;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class AgendaController extends Controller
 {
-    function test() 
+    function test()
     {
-        $agendas = Agenda::all()->map(function ($agenda){
+        $agendas = Agenda::all()->map(function ($agenda) {
 
             return [
-                "data" => $agenda->date,
+                "data" => Carbon::parse($agenda->date)->format('d-m-Y'),
                 "hora" => $agenda->hour,
                 "vagas" => $agenda->vacancy,
-                "preenchido" => $agenda->filled,
+                "vagas_preenchidas" => $agenda->filled,
             ];
 
+            /*  */
         });
 
         return response()->json($agendas);
     }
 
-    function send(Request $request)
+    public function send(Request $request)
     {
         $validated = $request->validate([
-            "id" => "required",        
+            "data" => "required",
+            "hora" => "required",
         ]);
 
-        $agenda = Agenda::find($request->id);
+        $dateRaw = $request->data;
+
+        if (preg_match('/^\d{2}\/\d{2}\/\d{4}$/', $dateRaw)) {
+            $carbonDate = Carbon::createFromFormat('d/m/Y', $dateRaw);
+        } elseif (preg_match('/^\d{4}-\d{2}-\d{2}$/', $dateRaw)) {
+            $carbonDate = Carbon::createFromFormat('Y-m-d', $dateRaw);
+        } elseif (preg_match('/^\d{4}\/\d{2}\/\d{2}$/', $dateRaw)) {
+            $carbonDate = Carbon::createFromFormat('Y/m/d', $dateRaw);
+        } elseif (preg_match('/^\d{2}-\d{2}-\d{4}$/', $dateRaw)) {
+            $carbonDate = Carbon::createFromFormat('d-m-Y', $dateRaw);
+        } else {
+            return response()->json(["error" => "Formato de data inválido."], 422);
+        }
+
+        $dateForQuery = $carbonDate->format('d-m-Y');
+
+        $agenda = Agenda::where('date', $dateForQuery)
+            ->where('hour', $request->hora)
+            ->first();
+
+        if (!$agenda) {
+            return response()->json(["error" => "Horário não encontrado."], 404);
+        }
+
+        if ($agenda->vacancy <= $agenda->filled) {
+            return response()->json(["error" => "Sem vagas para esse horário."], 400);
+        }
 
         $agenda->filled += 1;
         $agenda->save();
 
-        if($agenda == null){
-            return response()->json(["error" => "Horario não encontrado"], 400);
-        }
-
-        return response()->json([$agenda]);
-
+        return response()->json(["agenda", $agenda], 200);
     }
 }
